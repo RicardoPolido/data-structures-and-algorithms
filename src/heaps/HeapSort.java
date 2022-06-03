8c61b97b71bc2880619f797e618526b6f6f92024
RicardoPolido <ricardo.polido@outlook.com>
2022-06-03 15:16:33 -0300

    Heaps and PriorityQueue.

---
+++ src/heaps/HeapSort.java
@@ -0,0 +1,14 @@
+package heaps;
+
+/*
+    - We know the root has the largest value (max-heap)
+    - Swap root with last element in the array.
+    - Heapify the tree, but exclude the last node.
+    - After heapify, second largest element is at the root.
+    - Rinse and repeat.
+ */
+public class HeapSort {
+
+
+
+}
